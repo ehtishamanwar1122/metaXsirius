52a0bf0b4a6c22511be0c4b7039b3b1c75b91da1
Ehtisham Anwar <ehtishamanwar.1112@gmail.com>
2024-09-14 10:41:09 +0500

    changes

--- src/Components/Dashboard/DashboardHeader.js
+++ src/Components/Dashboard/DashboardHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DashboardHeader = ({ title, subtitle }) => (
   <div className='dashboard_head d-flex flex-column align-items-center text-center'>
-    <h1 className='font-weight-bold'>{title}</h1>
+    <h1 className='fw-bold'>{title}</h1>
     <p className='lead'>{subtitle}</p>
   </div>
 );
